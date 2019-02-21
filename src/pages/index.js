ed6d12b707eb0802a8858c32e0a506feb4633cff
byl <byl@iMac-de-byl-5.local>
2019-02-21 16:37:44 -0300

    commit inicial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { graphql, Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
@@ -15,7 +15,31 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <div>
+      <h1>My WordPress Sample Blog</h1>
+      <h4>Posts</h4>
+      {data.allWordpressPost.edges.map(({ node }) => (
+        <div>
+          <p>{node.title}</p>
+          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+        </div>
+      ))}
+    </div>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`;
